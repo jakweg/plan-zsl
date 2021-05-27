03410c02756e9754a183b638c6fb7ed5a1008c4d
jakweg <jakubek.weg@gmail.com>
2021-05-27 22:54:53 +0200

    Removed old backend

--- back/src/timetable/subject.ts
+++
@@ -1,9 +0,0 @@
-export class Subject {
-    constructor(public id: string,
-        public name: string,
-        public short: string) { }
-
-    static fromXml(xml: any): Subject {
-        return xml; // no name changes
-    }
-}

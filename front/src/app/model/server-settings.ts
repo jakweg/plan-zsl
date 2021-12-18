6d6c1dbbbf3a651560f0d97ad68522b9aa237091
jakweg <jakubek.weg@gmail.com>
2021-12-18 13:13:20 +0100

    Remove use new map option on frontend

--- front/src/app/model/server-settings.ts
+++ front/src/app/model/server-settings.ts
@@ -1,11 +1,8 @@
 
-export type SERVER_SETTING = 'use-new-map' | 'auto-rotation' | 'timetable-info-cache-duration';
+export type SERVER_SETTING = 'auto-rotation' | 'timetable-info-cache-duration';
 
 export class ServerSettings {
   public token: string;
-  public currentName: string;
-  public currentId: number;
   public rotationEnabled: boolean;
-  public useNewMap: boolean;
   public timetableCacheDuration: number;
 }
